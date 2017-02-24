f816e9ab38fdb358b246362b1ad7470163d82dde
siddeshshewde <siddesh.darkeyed95@gmail.com>
2017-02-24 21:27:01 +0530

    Add files via upload

---
+++ Programming-Practice/Java/Patterns/1/1.java
@@ -0,0 +1,16 @@
+public class 1
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = n.nextInt();
+		for (int i=0;i<n;i++)
+		{
+			for (int j=0;j<n;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
