27135d26a2900d3ebc5a2caddc559021b285b5d3
siddeshshewde <siddesh.darkeyed95@gmail.com>
2017-03-01 00:03:26 +0530

    Update Solution.java

--- HackerEarth-Solutions/Practice/Basic Programming/Implementation/Very-Easy/Little Jhool and Psychic Powers/Solution.java
+++ HackerEarth-Solutions/Practice/Basic Programming/Implementation/Very-Easy/Little Jhool and Psychic Powers/Solution.java
@@ -5,8 +5,7 @@ class TestClass
     public static void main(String args[] )
     {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        String text = Integer.toString(a);
+        String text = sc.nextLine();
         if(text.contains("000000") || text.contains("111111"))
             System.out.print("Sorry, sorry!");
         else
