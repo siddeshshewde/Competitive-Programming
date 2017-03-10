f626125abc592a10c95511268ff386e1a78c7c9d
siddeshshewde <siddesh.darkeyed95@gmail.com>
2017-03-11 02:30:03 +0530

    Add files via upload

---
+++ Techgig-Solutions/Practice/Pattern Problems/Numerical Rectangle/Solution.c
@@ -0,0 +1,13 @@
+//https://www.techgig.com/practice/question/numerical-rectangle/NTM2NUAjJEAjJDIwNjY4MDlAIyRAIyQzOTgwNTA5QCMkQCMkMTQ4OTE3OTA5MQ==/1
+
+/* Read input from STDIN. Print your output to STDOUT*/
+#include<stdio.h>
+int main()
+{
+	int i,n,j;
+	scanf("%d",&n);
+	for (i=1;i<=n;i++)
+	{
+	    printf("%d %d %d %d %d\n",i,i,i,i,i);
+	}
+}
