5acdbf60057f044394985fbf771569c57a6bee85
siddeshshewde <siddesh.darkeyed95@gmail.com>
2017-03-11 02:58:55 +0530

    Add files via upload

---
+++ Techgig-Solutions/Practice/Pattern Problems/Descending Alphabetical Rectangle/Solution.c
@@ -0,0 +1,13 @@
+//https://www.techgig.com/practice/question/descending-alphabetical-rectangle/NTM3MkAjJEAjJDIwNjY4MDlAIyRAIyQzOTgwNTQyQCMkQCMkMTQ4OTE4MDcyMg==/1
+
+/* Read input from STDIN. Print your output to STDOUT*/
+#include<stdio.h>
+int main(int argc, char *a[])
+{
+	int n,i;
+	scanf("%d",&n);
+	for (i=0;i<n;i++)
+	{
+	    printf("E D C B A\n");   
+	}
+}
