307ea52bee2ec7b4cbcd4d5849cc7cc7c8c97e68
siddeshshewde <siddesh.darkeyed95@gmail.com>
2017-03-11 02:34:11 +0530

    Add files via upload

---
+++ Techgig-Solutions/Practice/Pattern Problems/Ascending Numerical Rectangle/Solution.c
@@ -0,0 +1,13 @@
+//https://www.techgig.com/practice/question/ascending-number-rectangle/NTM2NkAjJEAjJDIwNjY4MDlAIyRAIyQzOTgwNTIwQCMkQCMkMTQ4OTE3OTY0NA==/1
+
+/* Read input from STDIN. Print your output to STDOUT*/
+#include<stdio.h>
+int main(int argc, char *a[])
+{
+	int n,i;
+	scanf("%d",&n);
+	for (i=0;i<n;i++)
+	{
+	    printf("1 2 3 4 5\n");
+	}
+}
