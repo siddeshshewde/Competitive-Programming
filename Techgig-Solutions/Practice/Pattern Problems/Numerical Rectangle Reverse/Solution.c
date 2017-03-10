2e7fd499b7dea20be0a903bb98e11861b533af9e
siddeshshewde <siddesh.darkeyed95@gmail.com>
2017-03-11 02:44:00 +0530

    Add files via upload

---
+++ Techgig-Solutions/Practice/Pattern Problems/Numerical Rectangle Reverse/Solution.c
@@ -0,0 +1,13 @@
+//https://www.techgig.com/practice/question/numerical-rectangle-reverse/NTM2OUAjJEAjJDIwNjY4MDlAIyRAIyQzOTgwNTMxQCMkQCMkMTQ4OTE4MDIyMw==/1
+
+/* Read input from STDIN. Print your output to STDOUT*/
+#include<stdio.h>
+int main(int argc, char *a[])
+{
+	int n,i;
+	scanf("%d",&n);
+	for (i=n;i>0;i--)
+	{
+	    printf("%d %d %d %d %d\n",i,i,i,i,i);
+	}
+}
