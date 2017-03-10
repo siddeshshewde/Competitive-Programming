677a4d8f244884e9952a6ad27929c60db99a3682
siddeshshewde <siddesh.darkeyed95@gmail.com>
2017-03-11 02:23:56 +0530

    Update Solution.c

--- Techgig-Solutions/Practice/Pattern Problems/Asterisk Rectangle/Solution.c
+++ Techgig-Solutions/Practice/Pattern Problems/Asterisk Rectangle/Solution.c
@@ -1,3 +1,5 @@
+//https://www.techgig.com/practice/question/asterisk-rectangle/NTM2NEAjJEAjJDIwNjY4MDlAIyRAIyQzOTgwNTA2QCMkQCMkMTQ4OTE3OTA1NA==/1
+
 /* Read input from STDIN. Print your output to STDOUT*/
 #include<stdio.h>
 void main()
