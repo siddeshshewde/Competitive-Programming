c4580bebcdbe2fe8bd77e75300c39e64abaa3c7f
siddeshshewde <siddesh.darkeyed95@gmail.com>
2017-03-16 23:22:12 +0530

    Update multiplyTest.py

--- Codewars-Solutions/Python/8 Kyu/Multiply/multiplyTest.py
+++ Codewars-Solutions/Python/8 Kyu/Multiply/multiplyTest.py
@@ -5,3 +5,8 @@
 #   test.assert_not_equals(actual, expected, [optional] message)
 
 # You can use Test.describe and Test.it to write BDD style test groupings
+
+Test.assert_equals(multiply(1,1), 1)
+Test.assert_equals(multiply(2,1), 2)
+Test.assert_equals(multiply(2,2), 4)
+Test.assert_equals(multiply(3,5), 15)
