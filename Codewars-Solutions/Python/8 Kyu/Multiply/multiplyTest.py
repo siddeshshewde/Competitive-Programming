1d60a61a65e6e0c63fbfc3871e8ff6e383b9579f
siddeshshewde <siddesh.darkeyed95@gmail.com>
2017-03-16 23:20:56 +0530

    Add files via upload

---
+++ Codewars-Solutions/Python/8 Kyu/Multiply/multiplyTest.py
@@ -0,0 +1,7 @@
+# TODO: Replace examples and use TDD development by writing your own tests
+# These are some of the methods available:
+#   test.expect(boolean, [optional] message)
+#   test.assert_equals(actual, expected, [optional] message)
+#   test.assert_not_equals(actual, expected, [optional] message)
+
+# You can use Test.describe and Test.it to write BDD style test groupings
