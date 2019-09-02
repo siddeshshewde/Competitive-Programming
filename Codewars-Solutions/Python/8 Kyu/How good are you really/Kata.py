34106a546b4ce421a2f6de47625238cdc9f9e320
siddeshshewde <siddesh.darkeyed95@gmail.com>
2019-09-02 19:50:33 +0530

    Add files via upload

---
+++ Codewars-Solutions/Python/8 Kyu/How good are you really/Kata.py
@@ -0,0 +1,6 @@
+def better_than_average(class_points, your_points):
+    n = (sum(class_points) + your_points) / (len(class_points) + 1)
+    if n > your_points: 
+        return False 
+    else: 
+        return True
