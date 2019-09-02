2afb26c68335cf33f6d7930b0d7e50bd29163978
siddeshshewde <siddesh.darkeyed95@gmail.com>
2019-09-02 19:53:06 +0530

    Update Kata.py

--- Codewars-Solutions/Python/8 Kyu/How good are you really/Kata.py
+++ Codewars-Solutions/Python/8 Kyu/How good are you really/Kata.py
@@ -4,3 +4,8 @@ def better_than_average(class_points, your_points):
         return False 
     else: 
         return True
+    
+    '''
+    One Liner :)
+    return your_points > sum(class_points) / len(class_points)
+    '''
