04264159c016d2ad821acbcc0535b070727e8053
siddeshshewde <siddesh.darkeyed95@gmail.com>
2019-09-15 02:14:26 +0530

    Add files via upload

---
+++ Reddit- Solutions/dailyprogrammer Challenges/[easy] Challenge #1/challenge1.py
@@ -0,0 +1,7 @@
+print("Please enter your Name, Age and Reddit Username")
+
+name = input("Name : ")
+age = input("Age : ")
+username = input("Reddit Username : ")
+
+print ("Your name is " +name+ ", you are " +age+ " years old, and your Reddit Username is " +username)
