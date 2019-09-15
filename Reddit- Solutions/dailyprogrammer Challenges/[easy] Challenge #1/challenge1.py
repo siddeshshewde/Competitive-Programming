19cb314aea143d1f9879bddce1402a9d84348d4d
siddeshshewde <siddesh.darkeyed95@gmail.com>
2019-09-15 12:09:33 +0530

    Update challenge1.py

--- Reddit- Solutions/dailyprogrammer Challenges/[easy] Challenge #1/challenge1.py
+++ Reddit- Solutions/dailyprogrammer Challenges/[easy] Challenge #1/challenge1.py
@@ -4,4 +4,10 @@ name = input("Name : ")
 age = input("Age : ")
 username = input("Reddit Username : ")
 
+
 print ("Your name is " +name+ ", you are " +age+ " years old, and your Reddit Username is " +username)
+
+f = open("test.txt","a+")
+
+f.write("\nYour name is " +name+ ", you are " +age+ " years old, and your Reddit Username is " +username)
+f.close()
