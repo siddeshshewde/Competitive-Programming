01ae190ad1e114f6944cb6c5f8296feb8cb77a04
siddeshshewde <siddesh.darkeyed95@gmail.com>
2019-09-15 12:10:34 +0530

    Update challenge1.py

--- Reddit- Solutions/dailyprogrammer Challenges/[easy] Challenge #1/challenge1.py
+++ Reddit- Solutions/dailyprogrammer Challenges/[easy] Challenge #1/challenge1.py
@@ -1,13 +1,14 @@
 print("Please enter your Name, Age and Reddit Username")
 
+#storing input for Name, Age and username
 name = input("Name : ")
 age = input("Age : ")
 username = input("Reddit Username : ")
 
-
+#printing the details
 print ("Your name is " +name+ ", you are " +age+ " years old, and your Reddit Username is " +username)
 
+#opening the text file and appending text
 f = open("test.txt","a+")
-
 f.write("\nYour name is " +name+ ", you are " +age+ " years old, and your Reddit Username is " +username)
 f.close()
