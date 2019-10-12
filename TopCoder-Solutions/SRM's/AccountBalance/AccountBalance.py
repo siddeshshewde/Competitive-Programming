56e8ff78bf41a6feacabc05c7f1f200cec305fb2
siddeshshewde <siddesh.darkeyed95@gmail.com>
2019-10-12 22:53:34 +0530

    Update AccountBalance.py

--- TopCoder-Solutions/SRM's/AccountBalance/AccountBalance.py
+++ TopCoder-Solutions/SRM's/AccountBalance/AccountBalance.py
@@ -1,12 +1,21 @@
 # Problem      : AccountBalance
 # Used In      : SRM 288
+# Date         : 02.08.2006  
+# Category     : Simple Math, String Parsing
 # Division     : 2
 # Level        : 1
+# Round        : 1  
 # Points       : 250
 # Difficulty   : Easy
 # Problem Type : Single
 # Description  : https://community.topcoder.com/stat?c=problem_statement&pm=6036
 
+# Class Name   : AccountBalance  
+# Method Name  : processTransactions   
+# Return Type  : Int  
+# Arg Types    : (tuple (integer))  
+
+#Solution
 class AccountBalance:
 	def processTransactions(balance, transactions):
 		for i in transactions:
@@ -15,9 +24,7 @@ class AccountBalance:
 			else: balance -= k[1]
 		return balance	
 
-
 # Points Received - 75.57
-
 
 #Split Function:
 #	split() function returns a list of strings after breaking the given string by the specified separator.
